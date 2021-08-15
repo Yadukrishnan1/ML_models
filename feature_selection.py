d2ba9eada1cec5772af927f555b0153d3f2a8261
Yadu Krishnan Sarathchandran <yadukrishnan786@gmail.com>
2021-08-15 16:51:10 -0500

    Update feature_selection.py

--- feature_selection.py
+++ feature_selection.py
@@ -1,1 +1,27 @@
+# Label encoding snippet for categorical variables in a dataframe
+
+X = df.copy()
+y = X.pop("price")
+
+# Label encoding for categoricals
+for colname in X.select_dtypes("object"):
+    X[colname], _ = X[colname].factorize()
+
+# All discrete features should now have integer dtypes (double-check this before using MI!)
+discrete_features = X.dtypes == int
+
+#-----------------------------------------------------------------------------------------------------------------------
+# MUtual Information from Scikit-learn
+
+from sklearn.feature_selection import mutual_info_regression
+
+def make_mi_scores(X, y, discrete_features):
+    mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+
+mi_scores = make_mi_scores(X, y, discrete_features)
+mi_scores[::3]  # show a few features with their MI scores
+
 
